c20c3f8dbf625c410f2d83d385ba42773afa2e3b
hyeonseo1109 <alsgustj2@naver.com>
2025-07-22 15:31:23 +0900

    FavoriteButton onClick함수 분리

--- src/component/FavoriteButton.jsx
+++ src/component/FavoriteButton.jsx
@@ -1,14 +1,35 @@
+// import { useDispatch, useSelector } from "react-redux";
+// import { favoriteSlice } from "../RTK/slice";
+
+// export default function FavoriteButton({ pokemonId }) {
+//     const isFavorite = useSelector(state => state.favorite.some((item => item === pokemonId)))
+//     const dispatch = useDispatch()
+    
+//     return (
+//         <button onClick={(e) => {
+//             e.stopPropagation()
+//             dispatch(isFavorite ? favoriteSlice.actions.removeFromFavorite({pokemonId}) : favoriteSlice.actions.addToFavorite({pokemonId}))
+//         }}
+//             className={`bg-transparent ${isFavorite ? "text-[#1cd2ba]" : 'text-[#aaa]'}`} >
+//             {isFavorite ? '♥︎' : '♡'}
+//         </button>
+//     )
+// }
+
 import { useDispatch, useSelector } from "react-redux";
 import { favoriteSlice } from "../RTK/slice";
 
 export default function FavoriteButton({ pokemonId }) {
     const isFavorite = useSelector(state => state.favorite.some((item => item === pokemonId)))
     const dispatch = useDispatch()
+    const handleClick = (e) => {
+        e.stopPropagation()
+        if (isFavorite) dispatch(favoriteSlice.actions.removeFromFavorite({pokemonId}))
+        else dispatch(favoriteSlice.actions.addToFavorite({pokemonId}))
+        }
+
     return (
-        <button onClick={(e) => {
-            e.stopPropagation()
-            dispatch(isFavorite ? favoriteSlice.actions.removeFromFavorite({pokemonId}) : favoriteSlice.actions.addToFavorite({pokemonId}))
-        }}
+        <button onClick={(e) => {handleClick(e)}}
             className={`bg-transparent ${isFavorite ? "text-[#1cd2ba]" : 'text-[#aaa]'}`} >
             {isFavorite ? '♥︎' : '♡'}
         </button>
